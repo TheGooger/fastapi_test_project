fc1d6e428c568b363447707ae5f8b49985d4a09e
TheGooger <vladimir.ivanov1307@gmail.com>
2025-12-06 16:20:45 +0300

    Created CRUD and all things for database interaction (table products)

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from fastapi import FastAPI
 
 import uvicorn
 
+from api_v1 import router as router_v1
 from core.models import Base, db_helper
+from core.config import settings
 from items_views import router as items_router
 from users.views import router as users_router
 
@@ -20,6 +22,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(users_router)
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
 
 
 @app.get("/")
