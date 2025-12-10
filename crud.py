046a7b823a790cd2ad8d1d4d9a35046e53b9df6e
TheGooger <vladimir.ivanov1307@gmail.com>
2025-12-10 19:52:11 +0300

    made select for users

--- crud.py
+++ crud.py
@@ -1,4 +1,7 @@
 import asyncio
+
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper, User, Profile, Post
 
@@ -11,10 +14,22 @@ async def create_user(session: AsyncSession, username: str) -> User:
     return user
 
 
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    stmt = select(User).where(User.username == username)
+    # result: Result = await session.execute(stmt)
+    # user: User | None = result.scalar_one_or_none()
+    # user: User | None = result.scalar_one()
+    user: User | None = await session.scalar(statement=stmt)
+    print("found user", username, user)
+    return user
+
+
 async def main():
     async with db_helper.session_factory() as session:
-        await create_user(session=session, username="john")
-        await create_user(session=session, username="sam")
+        # await create_user(session=session, username="john")
+        # await create_user(session=session, username="sam")
+        await get_user_by_username(session, "sam")
+        await get_user_by_username(session, "bob")
 
 
 if __name__ == "__main__":
